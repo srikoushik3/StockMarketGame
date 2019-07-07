b04bbde7708690561aaa7a21d3211bb0d3f6a0e7
Sri Koushik Nellutla <sri@en-powered.com>
2019-07-07 13:18:42 -0400

    JSON support for Users

--- UserManager.cc
+++ UserManager.cc
@@ -3,3 +3,17 @@
 #include <memory>
 #include <string>
 using namespace std;
+
+UserManager::UserManager(){}
+
+UserManager::createUsersFromUsername(vector<string> usernames){
+  for(int i = 0; i<usernames.size(); ++i){
+    addUser(usernames[i]);
+  }
+}
+
+UserManager::addUser(string username){
+  // if User DNE -> add user
+  if()
+  users.insert(make_pair<string, unique_ptr<User>>(usernames[i], make_unique<User>(usernames[i])));
+}
--- UserManager.h
+++ UserManager.h
@@ -7,8 +7,8 @@ class UserManager{
   std::unordered_map<string, unique_ptr<User>> users;
   public:
     UserManager();
-    void addUser();
-    void removeUser();
+    void addUser(std::string);
+    void removeUser(std::string);
     void SaveGameForAllUsers();
     void loadUsersFromFile(fstream&);
     void addShares(string, int, float);
