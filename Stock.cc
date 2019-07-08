7f4766f6234718643483b04306bbe9e4679e2754
Shisheer <shisheer.choudhary@gmail.com>
2019-07-08 18:26:42 -0400

    Implemented EDOStockPrices and Added Comments

--- Stock.cc
+++ Stock.cc
@@ -7,22 +7,31 @@
 using json = nlohmann::json;
 using namespace std;
 
+// Produces seed only once when new instance of Stock class is made
 Stock::Stock() {
   srand(unsigned(time(NULL)));
 }
 
+// buyShares is called by buyShares in StockManager Class
+// Removes the stocks bought from the number of stocks available
 void Stock::buyShares(int numSharesPurchased) {
   numShares -= numSharesPurchased;
 }
 
+// sellShares is called by sellShares in StockManager Class
+// Adds the stocks sold to the number of stocks available
 void Stock::sellShares(int numSharesSold) {
   numShares += numSharesSold;
 }
 
+// hasSufficentShares is called by hasSufficentShares in StockManager Class
+// Checks to see if user is trying to buy more than what is available
 bool Stock::hasSufficentShares(int numSharesToBePurchased) {
   return (numShares - numSharesToBePurchased < 0) ? false : true;
 }
 
+// setEODStockPrice is called by setEODStockPrices in StockManager Class
+// Uses Variance to calculate the Stock price when user proceeds to next trading day
 void Stock::setEODStockPrice() {
   openingPricePerShare += (float)rand()/(RAND_MAX)*(2*maxPriceVariance)-maxPriceVariance;
 }
