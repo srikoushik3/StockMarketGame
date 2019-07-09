fe8eaea7e31868aeb51fe5c51d68342aae97b0a1
nimwijetunga <nim.wijetunga@gmail.com>
2019-07-09 14:36:56 -0400

    stuff

--- newgamefilemanager.cpp
+++ newgamefilemanager.cpp
@@ -15,6 +15,7 @@ NewGameFileManager::NewGameFileManager(QWidget *parent) :
     ui(new Ui::NewGameFileManager),
     numUsers(0), daysPerTurn(0), totalDays(0), stocksFileName("stocks.json")
 {
+    //Set Labels to 0
     ui->setupUi(this);
     ui->numUsersLbl->setText("0");
     ui->daysLbl->setText("0");
@@ -29,29 +30,34 @@ NewGameFileManager::~NewGameFileManager()
 
 void NewGameFileManager::on_numUsersBtn_clicked()
 {
+    //Save number of users
     numUsers = QInputDialog::getInt(this, "Number of Users", "Enter Number of Users");
     ui->numUsersLbl->setText(QString::number(numUsers));
 }
 
 void NewGameFileManager::on_daysPerTurnBtn_clicked()
 {
+    // Save days per turn
     daysPerTurn = QInputDialog::getInt(this, "Days Per Turn", "Enter Days Per Turn");
     ui->daysLbl->setText(QString::number(daysPerTurn));
 }
 
 void NewGameFileManager::on_totalDaysBtn_clicked()
 {
+     // Save total days
     totalDays = QInputDialog::getInt(this, "Total Days", "Enter Total Days");
     ui->totalDaysLbl->setText(QString::number(totalDays));
 }
 
 void NewGameFileManager::on_importStocksBtn_clicked()
 {
+    // Promt user for stocks file
     stocksFileName = QFileDialog::getOpenFileName(this, tr("Open File"), "C://", "Json File(*.json)").toStdString();
 }
 
 void NewGameFileManager::on_submitBtn_clicked()
 {
+     // Make sure no fields are empty
     if(numUsers == 0 || daysPerTurn == 0 || totalDays == 0 || stocksFileName == ""){
         ui->errorLabel->setText("Please Enter a Value for All Fields");
         return;
